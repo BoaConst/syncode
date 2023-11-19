230228c8e0de02fa429f915d3d2a71757cae7005
BoaConst <suumilroy120@gmail.com>
2023-11-19 16:35:15 -0500

    refactored demins interfaces

---
+++ src/machine_hiding/file_system_operations/mod.rs
@@ -0,0 +1,95 @@
+/**
+ * Module A.1 : FileSystem Operations Module
+ * Designer : Demin 
+ * Reviewer Suumil
+ */
+mod file_system_operations {
+    pub struct FileSystemOperationsModule {
+        // Additional fields can be added as needed
+    }
+
+    #[derive(Debug, PartialEq)]
+    pub enum FileSystemConflict {
+        LineEndings,
+        PathFormat,
+        CharacterEncoding,
+        // Add other specific conflicts as and when discovered while testing
+        Unknown, 
+    }
+
+    #[derive(Debug, PartialEq)]
+    pub enum IoError {
+        InvalidPath,
+        ConflictDetectionFailed,
+        OperationAdaptationFailed,
+        // Other I/O errors can be added as required
+    }
+
+    impl FileSystemOperationsModule {
+        // Creates a new `FileSystemOperationsModule` instance.
+        pub fn new() -> Self {
+            FileSystemOperationsModule {
+            }
+        }
+
+        // Identifies filesystem conflicts.
+        pub fn identify_conflict(&self, file_path: &str) -> Result<FileSystemConflict, IoError> {
+            // TODO: Implement conflict identification logic
+            // This is a mock implementation. Replace with actual logic.
+            if file_path.is_empty() {
+                Err(IoError::InvalidPath)
+            } else {
+                Ok(FileSystemConflict::Unknown) // Replace with actual conflict detection
+            }
+        }
+
+        // Adapts I/O operations to handle identified conflicts.
+        pub fn adapt_io_operations(&self, file_path: &str, conflict: FileSystemConflict) -> Result<(), IoError> {
+            // TODO: Implement I/O operation adaptation logic, replace the placeholder mock logic
+            match conflict {
+                FileSystemConflict::Unknown => Err(IoError::OperationAdaptationFailed),
+                _ => Ok(()),
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::file_system_operations::*;
+    use super::*;
+
+    #[test]
+    fn conflict_identification_success() {
+        let file_system_module = FileSystemOperationsModule::new();
+        let file_path = "/path/to/file";
+        let result = file_system_module.identify_conflict(file_path);
+        assert!(result.is_ok());
+    }
+
+    #[test]
+    fn conflict_identification_failure() {
+        let file_system_module = FileSystemOperationsModule::new();
+        let invalid_file_path = "";
+        let result = file_system_module.identify_conflict(invalid_file_path);
+        assert_eq!(result, Err(IoError::InvalidPath));
+    }
+
+    #[test]
+    fn io_operation_adaptation_success() {
+        let file_system_module = FileSystemOperationsModule::new();
+        let file_path = "/path/to/file";
+        let conflict = FileSystemConflict::LineEndings;
+        let result = file_system_module.adapt_io_operations(file_path, conflict);
+        assert_eq!(result, Ok(()));
+    }
+
+    #[test]
+    fn io_operation_adaptation_failure() {
+        let file_system_module = FileSystemOperationsModule::new();
+        let file_path = "/path/to/file";
+        let invalid_conflict = FileSystemConflict::Unknown;
+        let result = file_system_module.adapt_io_operations(file_path, invalid_conflict);
+        assert_eq!(result, Err(IoError::OperationAdaptationFailed));
+    }
+}
---
+++ src/repository_hiding/inspection/mod.rs
@@ -0,0 +1,133 @@
+/**
+ * Module D.2 : Inspection Module
+ * Designer : Demin 
+ * Reviewer Helia
+ */
+mod inspection_module {
+    pub struct InspectionModule {
+        
+    }
+
+    pub enum HeadInfo {
+        Branch(String),
+        Error(String),
+    }
+
+    impl InspectionModule {
+        // Creates a new `InspectionModule` instance.
+        pub fn new() -> Self {
+            InspectionModule {
+
+            }
+        }
+
+        // Checks the current head of the repository.
+        pub fn check_current_head(repo_path: &str) -> HeadInfo {
+            // TODO: Implement head checking logic
+            if repo_path.is_empty() {
+                HeadInfo::Error("Invalid repository path".to_string())
+            } else {
+                HeadInfo::Branch("master".to_string()) 
+            }
+        }
+
+        // Compares the difference between revisions.
+        pub fn compare_diff(file_path: &str, old_rev: &str, new_rev: &str) -> Result<String, String> {
+            // TODO: Implement diff logic
+            Ok("diff content".to_string())
+        }
+
+        // Inspects a file in a specific revision.
+        pub fn inspect_file(repo_path: &str, revision: &str, file_path: &str) -> Result<String, String> {
+            // TODO: Implement file inspection logic
+            Ok("file content".to_string())
+        }
+
+        // Checks out a specific revision.
+        pub fn checkout_revision(repo_path: &str, revision_hash: &str) -> Result<(), String> {
+            // TODO: Implement checkout logic
+            if revision_hash.len() != 40 { 
+                Err("Invalid hash".to_string())
+            } else {
+                Ok(())
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::inspection_module::*;
+
+    #[test]
+    fn head_success() {
+        let result = InspectionModule::check_current_head("sample_repo");
+        match result {
+            HeadInfo::Branch(branch) => assert_eq!(branch, "master"),
+            HeadInfo::Error(_) => panic!("Expected HeadInfo::Branch, got HeadInfo::Error."),
+        }
+    }
+
+    #[test]
+    fn head_failure() {
+        let result = InspectionModule::check_current_head("");
+        match result {
+            HeadInfo::Error(error_message) => assert_eq!(error_message, "Invalid repository path"),
+            _ => panic!("Expected HeadInfo::Error, got something else."),
+        }
+    }
+
+    #[test]
+    fn diff_success() {
+        let result = InspectionModule::compare_diff("sample.txt", "HEAD^", "HEAD");
+        match result {
+            Ok(diff_content) => assert!(diff_content.contains("diff content")),
+            Err(error) => panic!("Unexpected error: {}", error),
+        }
+    }
+
+    #[test]
+    fn diff_success_same() {
+        let result = InspectionModule::compare_diff("sample.txt", "HEAD", "HEAD");
+        match result {
+            Ok(diff_content) => assert!(diff_content.contains("diff content")),
+            Err(error) => panic!("Unexpected error: {}", error),
+        }
+    }
+
+    #[test]
+    fn inspect_existing_file() {
+        let repo_path = "sample_repo";
+        let result = InspectionModule::inspect_file(repo_path, "HEAD", "sample.txt");
+        match result {
+            Ok(content) => assert_eq!(content, "file content"),
+            Err(error) => panic!("Unexpected error: {}", error),
+        }
+    }
+
+    #[test]
+    fn inspect_non_existing_file() {
+        let repo_path = "sample_repo";
+        let result = InspectionModule::inspect_file(repo_path, "HEAD", "non_existing.txt");
+        match result {
+            Ok(content) => panic!("Expected error, but got content: {}", content),
+            Err(error) => assert!(true), // Error is expected
+        }
+    }
+
+    #[test]
+    fn checkout_valid_hash() {
+        let repo_path = "sample_repo";
+        let valid_hash = "0123456789abcdef0123456789abcdef01234567";
+        let result = InspectionModule::checkout_revision(repo_path, valid_hash);
+        assert!(result.is_ok());
+    }
+
+    #[test]
+    fn checkout_invalid_hash() {
+        let repo_path = "sample_repo";
+        let invalid_hash = "0123456789";
+        let result = InspectionModule::checkout_revision(repo_path, invalid_hash);
+        assert_eq!(result, Err("Invalid hash".to_string()));
+    }
+}
